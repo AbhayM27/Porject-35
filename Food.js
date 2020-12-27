499d146bc14e8fc2ca37f8c6d402b4d2ebef94ff
AbhayM27 <72178325+AbhayM27@users.noreply.github.com>
2020-12-26 20:03:43 -0800

    Update Food.js

--- Food.js
+++ Food.js
@@ -10,41 +10,37 @@ class Food {
 
 
 getFoodStock() {
- var foodStock = database.ref('Food');
+ foodStock = database.ref('Food');
  foodStock.on("value",function(data){
     foodStock = data.val();
 })
 }
 
-updateFoodStock(x){
+updateFoodStock(){
     database.ref('/').update({
-    Food: x 
+    Food:foodS
 })
     
  
 }
 
 deductFood() {
-if(x<=0) {
-    x=0;
-  } else {
-    x=x-1;
-  }
+    foodS=foodS-1;
 }
 
 display() {
     var x=80, y=100;
 
     imageMode(CENTER);
-    image(this.image,720,220,70,70);
+    image(this.image,8000,220,70,70);
     
-    if(this.foodStock!=0) {
-        for(var i=0; i<this.foodStock;i++){
+    if(foodS!=0) {
+        for(var i=0; i<foodS; i++){
             if(i%10==0) {
                 x=80;
-                y=y+50;
+                y=y+80;
             }
-            this.image(this.image,x,y,70,70);
+            image(this.image,x,y,70,70);
             x=x+30;
         }
     }
